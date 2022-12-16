ce38ab43a8dd1b750a88f24c46b68c43eed48c64
Martin Arreguin <arrec7281@gmail.com>
2022-12-16 11:27:35 -0600

    Se modifico la clase tienda

--- src/PaqueteNumeroUno/Tienda.java
+++ src/PaqueteNumeroUno/Tienda.java
@@ -6,17 +6,39 @@ import java.util.Stack;
 
 public class Tienda {
 
-    public static void main(String... args) {
-
+    public static void main(String... args) throws IOException {
+        Tienda ob1 = new Tienda();
+        ArrayList<Tenis> listaTenis = new ArrayList();
+        
+        listaTenis = ob1.listaTenis();
+        
+        
     }
 
-    public ArrayList<Tenis> listaTenis() throws IOException {
+    private ArrayList<Tenis> listaTenis() throws IOException {
         Agregar ob1 = new Agregar();
         
         Stack<String> pila = new Stack();
         ArrayList<Tenis> listaTenis = new ArrayList();
         
+        String[] subCadenas;
+        String cadena;
+        String tenis;
+        double precio;
+        
         pila = ob1.agregar();
+        
+        while(!pila.isEmpty()){
+            cadena = pila.pop();
+            
+            subCadenas = cadena.split(" ");
+            
+            tenis = subCadenas[0];
+            precio = Double.parseDouble(subCadenas[1]);
+            
+            
+            listaTenis.add(new Tenis(tenis , precio));
+        }
         
         return listaTenis;
     }
