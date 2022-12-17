d4fc03d9e7364cadc9dd9454503873bc0c13238e
Martin Arreguin <arrec7281@gmail.com>
2022-12-17 09:14:30 -0600

    Se agrego los metodos de ordenamineto

--- src/PaqueteNumeroUno/Tienda.java
+++ src/PaqueteNumeroUno/Tienda.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Stack;
+import java.util.Collections;
 
 public class Tienda {
 
@@ -56,10 +58,36 @@ public class Tienda {
                         + "\n3.Ordenar por precio del menor al mayor"
                         + "4.Salir");
                 System.err.print("R=");
+                try{
+                    
+                }catch(NumberFormatException e){
+                    System.out.println("DEBES DE DIGITAR UN NUMERO INTENTA DE NUEVO");
+                }
                 n = Integer.parseInt(entrada = bufer.readLine());
             } while (n < 1 || n > 5);
+            
+            switch(n){
+                case 1:
+                    ordenarPorOrdenAlfabetico(lista);
+                    break;
+                case 2:
+                    ordenarPorPrecioMayor(lista);
+                    break;
+                case 3:
+                    break;
+            }
 
         } while (x == false);
 
     }
+    
+    private ArrayList<Tenis> ordenarPorOrdenAlfabetico(ArrayList<Tenis> lista){
+        Collections.sort(lista , (Tenis uno , Tenis dos) -> (uno.getTenis()).compareTo(dos.getTenis()));
+        return lista;
+    }
+    
+    private ArrayList<Tenis> ordenarPorPrecioMayor(ArrayList<Tenis> lista){
+        Collections.sort(lista , (Tenis uno , Tenis dos) -> new Double (uno.getPrecio()).compareTo(dos.getPrecio()));
+        return lista;
+    }  
 }
